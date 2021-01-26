992cb90cd14d42970e460151ccaccb6058d9dfab
Vladimir Timofeenko <id@vtimofeenko.com>
2021-01-25 16:44:48 -0800

    added async autosuggest

--- configs/rc/04-custom-tab-complete.zsh
+++ configs/rc/04-custom-tab-complete.zsh
@@ -4,5 +4,6 @@ bindkey -M menuselect 'h' vi-backward-char
 bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'l' vi-forward-char
 bindkey -M menuselect 'j' vi-down-line-or-history
+bindkey -M menuselect '^ ' accept-line
 
 bindkey -M menuselect "+" accept-and-menu-complete
