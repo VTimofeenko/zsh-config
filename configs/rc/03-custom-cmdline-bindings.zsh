cb3b2809212dfe794b0a39fba3c38e36a4affa0b
Vladimir Timofeenko <id@vtimofeenko.com>
2021-02-15 11:46:00 -0800

    added conditional fzf alias

--- configs/rc/03-custom-cmdline-bindings.zsh
+++ configs/rc/03-custom-cmdline-bindings.zsh
@@ -1,5 +1,11 @@
 # Enable fzf bindings for history and navigation. May be Gentoo-specific.
-source_if_exists ${ZSH_FZF_BINDINGS_PATH:-/usr/share/fzf/key-bindings.zsh}
+# Loads only if "fzf" is truly a command
+if [ command -v fzf > /dev/null 2>&1 ]; then
+	source_if_exists ${ZSH_FZF_BINDINGS_PATH:-/usr/share/fzf/key-bindings.zsh}
+else
+	# Fall back to default ctrl+R
+	bindkey "^R" history-incremental-pattern-search-backward
+fi
 
 bindkey "^A" vi-beginning-of-line
 bindkey "^E" vi-end-of-line
